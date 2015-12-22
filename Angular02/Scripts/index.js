65a6b24d192c6d43da2bf5fe4907e3c268a02bbb
zlinker89 <ospi89@hotmail.com>
2015-12-22 11:34:34 -0500

    se ha añadido una lista, por include
    practica de include y listas

---
+++ Angular02/Scripts/index.js
@@ -0,0 +1,30 @@
+﻿Array.prototype.remove = function (value) {
+    var idx = this.indexOf(value);
+    if (idx != -1) {
+        return this.splice(idx, 1); // The second parameter is the number of elements to remove.
+    }
+    return false;
+}
+var personas = [];
+
+angular.module("eva", []).controller("control1", function ($scope) {
+
+    $scope.addPersona = function () {
+        var p = {
+            nombres: $scope.nombres,
+            apellidos: $scope.apellidos
+        };
+        $scope.nombres = "";
+        $scope.apellidos = "";
+
+        personas.push(p);
+        $scope.personas = personas;
+    }
+
+    $scope.deletePersona = function (p) {
+        personas.remove(p);
+        $scope.personas = personas;
+    }
+
+
+});
