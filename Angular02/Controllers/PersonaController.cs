56c4100a3874f47f2ec671717455eddabe7aea92
evavides <eva.z0301@gmail.com>
2015-12-23 10:30:52 -0500

    DTO
    CREACION DE CONTROLADOR Y PERSONA DTO

---
+++ Angular02/Controllers/PersonaController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Angular02.Models;
+using Angular02.DTO;
+
+namespace Angular02.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        private ConsecionarioEntities db = new ConsecionarioEntities();
+
+        // GET api/Persona
+        public IQueryable<PersonaDTO> GetPersona()
+        {
+            var personas = from p in db.Persona
+                           select new PersonaDTO()
+                           {
+                               idpersona = p.idpersona,
+                               nombre = p.nombre,
+                               apellido = p.apellido,
+                               tdocumento = p.tdocumento,
+                               docmento = p.docmento,
+                               fecha_nacimiento = p.fecha_nacimiento
+
+                           };
+            return personas;
+        }
+
+        // GET api/Persona/5
+        [ResponseType(typeof(Persona))]
+        public async Task<IHttpActionResult> GetPersona(long id)
+        {
+            Persona persona = await db.Persona.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(persona);
+        }
+
+        // PUT api/Persona/5
+        public async Task<IHttpActionResult> PutPersona(long id, Persona persona)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != persona.idpersona)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(persona).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Persona
+        [ResponseType(typeof(Persona))]
+        public async Task<IHttpActionResult> PostPersona(Persona persona)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Persona.Add(persona);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = persona.idpersona }, persona);
+        }
+
+        // DELETE api/Persona/5
+        [ResponseType(typeof(Persona))]
+        public async Task<IHttpActionResult> DeletePersona(long id)
+        {
+            Persona persona = await db.Persona.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            db.Persona.Remove(persona);
+            await db.SaveChangesAsync();
+
+            return Ok(persona);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonaExists(long id)
+        {
+            return db.Persona.Count(e => e.idpersona == id) > 0;
+        }
+    }
+}
---
+++ Angular02/DTO/PersonaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular02.DTO
+{
+    public class PersonaDTO
+    {
+
+        public long idpersona { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string tdocumento { get; set; }
+        public string docmento { get; set; }
+        public Nullable<System.DateTime> fecha_nacimiento { get; set; }
+    }
+}
